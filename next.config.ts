5f492a0eb27457411d315342dd5f467b027bc448
unknown <wushenlunzhe007@163.com>
2026-08-01 02:05:45 +0800

    fix: allowedDevOrigins and serverExternalPackages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: [
+    "192.168.1.120",
+    "192.168.1.100",
+    "localhost",
+  ],
+  serverExternalPackages: ["bcrypt"],
 };
 
 export default nextConfig;
